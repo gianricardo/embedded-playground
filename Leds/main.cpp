93943f6e70f1eea7109991cd630fcef7e803c588
gian <gian.ricardo@gmail.com>
2017-07-10 18:43:37 -0300

    código do inicio da parte 2.

--- Leds/main.cpp
+++ Leds/main.cpp
@@ -4,15 +4,13 @@ int main(){
   WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
   P1DIR |= BIT0;                            // Set P1.0 to output direction
 
-  const auto toggle = [](auto& p, const auto bit){
+  const auto toggle = [](auto &p, const auto bit){
       p ^= (BIT0 << bit);
   };
 
   while (true){
-
       toggle(P1OUT, !BIT0);
-
-      for (volatile unsigned int i=25000;i!=0;i++){}
+      for (volatile unsigned int i=20000;i!=0;i--){}
   }
 }
 
